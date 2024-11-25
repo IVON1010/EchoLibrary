f9bd603788b54a6e91e79416f255c46fd0c7c417
Yvonne <ndunguyvonne10@gmail.com>
2024-11-25 16:19:11 +0300

    add inventoryResource and parser

--- resources/inventory.py
+++ resources/inventory.py
@@ -5,3 +5,18 @@ class InventoryResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('book_id', required=True, type=int, help='book_id is required')
     parser.add_argument('current_stock', required=True, type=int, help='current_stock is required')
+
+
+    def get(self, id=None):
+        if id:
+            inventory = Inventory.query.filter_by(id=id).first()
+            
+            if inventory == None:
+                return { 'message': 'Inventory not found'},404 
+            
+            return inventory.as_dict()
+        
+        else:
+            inventories = Inventory.query.all()
+            return [inventory.as_dict() for inventory in inventories]
+    
