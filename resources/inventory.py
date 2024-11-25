445d691289c7d750e42b809772e64b324810da35
Yvonne <ndunguyvonne10@gmail.com>
2024-11-25 16:18:44 +0300

    add inventoryResource and parser

--- resources/inventory.py
+++ resources/inventory.py
@@ -1,2 +1,7 @@
 from flask_restful import Resource, reqparse
 from models import db, Inventory
+
+class InventoryResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('book_id', required=True, type=int, help='book_id is required')
+    parser.add_argument('current_stock', required=True, type=int, help='current_stock is required')
